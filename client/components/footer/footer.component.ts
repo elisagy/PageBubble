6ad12962d174320fa250d96bd7fd5e657ad77ee2
Eli Sagy <sagyeli@gmail.com>
2020-04-28 21:40:14 +0300

    Several visualization bugs fixes

--- client/components/footer/footer.component.ts
+++ client/components/footer/footer.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'footer',
     template: require('./footer.html'),
     styles: [require('./footer.scss')]
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+	thisYear: Number;
+
+	ngOnInit() {
+		this.thisYear = (new Date()).getFullYear();
+	}
+}
